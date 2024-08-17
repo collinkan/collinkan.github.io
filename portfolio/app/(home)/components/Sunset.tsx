45e7ad59467f843bd60e906eb98aedc1524c7623
Collin Kan <collin.kan@gmail.com>
2024-08-17 02:12:18 -0700

    Add sunset component

--- portfolio/app/(home)/components/Sunset.tsx
+++ portfolio/app/(home)/components/Sunset.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
 export default function Sunset() {
-  return (
-    <div>Sunset</div>
-  )
+    const numDivisions = 7
+    const divisions = Array.from(Array(numDivisions).keys())
+
+    return (
+        <div className='absolute w-full flex justify-center py-52'>
+            <div className='absolute w-52 h-52 rounded-full'>
+                <span className="absolute -inset-5 bg-synthPink opacity-80 rounded-full blur-3xl"></span>
+            </div>
+            <div className='relative w-52 h-52 rounded-full overflow-hidden'>
+                <span className='absolute inset-0 bg-gradient-to-b from-synthYellow via-synthOrange to-synthRed'>
+                </span>
+                {/* <span>
+                    {divisions.map((division) => {
+                        return (
+                            <span>
+                                <span key={division} style={{top: `${division * (50/numDivisions) + 50}%`, height: 3 + division*0.5}} 
+                                    className={`absolute inset-x-0 bg-synthBG`}>
+                                </span>
+                                <span key={division} style={{top: `${division * (50/numDivisions) + 50}%`, height: 1 + division*0.5}}
+                                    className={`absolute inset-x-0 bg-synthPink blur-sm`}>
+                                </span>
+                            </span>
+                        )
+                    })}
+                </span> */}
+            </div>
+        </div>
+    )
 }
--- portfolio/app/(home)/page.tsx
+++ portfolio/app/(home)/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Navbar from "./components/Navbar"
 import Intro from "./components/HeroSection";
+import Sunset from "./components/Sunset";
 
 export default function page() {
   return (
     <div className="min-h-screen bg-synthBG">
       <div className="max-w-7x1 mx-auto">
+        <Sunset />
         <Navbar />
         <Intro />
       </div>
