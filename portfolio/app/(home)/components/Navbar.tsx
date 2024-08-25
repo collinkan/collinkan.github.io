733960e258bec2092c8aee622813c2017972b9ca
Collin Kan <collin.kan@gmail.com>
2024-08-24 19:00:51 -0700

    Implement Navbar component

--- portfolio/app/(home)/components/Navbar.tsx
+++ portfolio/app/(home)/components/Navbar.tsx
@@ -18,22 +18,36 @@ export default function page() {
         }
     ]
 
-  return (
-    <nav className="p-10 flex justify-between items-center bg-synthGrid text-synthText">
-        <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2">
-            Collin Kan
-        </h1>
-        <span className="flex items-center gap-5">
-            {socials.map((social, index) => {
-                const Icon = social.Icon
+    return (
+        <nav className="p-4 w-full fixed flex justify-between items-center shadow-lg shadow-synthBG/50 bg-synthTransparent backdrop-blur-md text-synthText">
+            <h1 className="px-8 w-1/3 text-2xl font-bold">
+                Collin Kan
+            </h1>
 
-                return (
-                    <Link href={social.link} key={index} aria-label={social.label}>
-                        <Icon className="w-5 h-5 hover:scale-125 transition-all"/>
-                    </Link>
-                )
-            })}
-        </span>
-    </nav>
-  )
+            <div className="w-1/3 h-auto overflow-auto flex justify-between items-center border border-synthTransparent/20 bg-[#1a1a1a2a] px-10 py-4 rounded-3xl text-synthText">
+                <a href="#about-me" className="w-1/3 size-auto text-left whitespace-nowrap cursor-pointer hover:scale-125 transition-all">
+                    About Me
+                </a>
+                <a href="#skills" className="w-1/3 text-center cursor-pointer hover:scale-125 transition-all">
+                    Skills
+                </a>
+                <a href="#experience" className="w-1/3 text-right cursor-pointer hover:scale-125 transition-all">
+                    Experience
+                </a>
+            </div>
+
+            <span className="px-8 w-1/3 flex justify-end items-center gap-5">
+                {socials.map((social, index) => {
+                    const Icon = social.Icon
+
+                    return (
+                        <Link className="group" href={social.link} key={index} aria-label={social.label}>
+                            <span className="absolute w-5 h-5 bg-synthTeal opacity-60 blur-sm rounded-lg group-hover:scale-150 transition-all"></span>
+                            <Icon className="relative w-5 h-5 group-hover:scale-125 transition-all" />
+                        </Link>
+                    )
+                })}
+            </span>
+        </nav>
+    )
 }
